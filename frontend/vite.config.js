ee94c83194d4cd846387cef7932ca22dace258f8
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-04-02 15:42:05 +0700

    xóa dòng thừa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3500',
-        changeOrigin: true,
-      }
-    }
-  }
+  
 })
